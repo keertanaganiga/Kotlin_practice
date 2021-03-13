9f3709f5281b8ac58158d4977a22f6e53f7e692f
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2021-03-13 05:33:57 -0800

    Create happy_birthday.kt

---
+++ happy_birthday.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    val age = 26
+    val name = "Kim taehyung"
+    
+    println("Happy Birthday, ${name}!")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+}
