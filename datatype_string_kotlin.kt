53a323af5af8f6c93d7a3a0d628eade07325bc1d
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2021-03-13 21:24:11 -0800

    Create datatype_string_kotlin.kt

---
+++ datatype_string_kotlin.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val border="*"
+    printBorder(border)
+    println("BTS kpop band")
+    printBorder(border)
+}
+
+fun printBorder(border:String){
+    repeat(23){
+        print(border)
+    }
+    println()
+}
